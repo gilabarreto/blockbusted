a57df4177445fd0bbe3609029e50d05af9c213e4
Victor Gila Barreto <gilabarreto@gmail.com>
2022-12-30 18:27:21 -0700

    Create welcome function and sleep helper function

--- index.js
+++ index.js
@@ -6,3 +6,29 @@ import gradient from 'gradient-string';
 import chalkAnimation from 'chalk-animation';
 import figlet from 'figlet';
 import { createSpinner } from 'nanospinner';
+
+console.log(chalk.bgGreen('Hello World!'));
+
+let playerName;
+
+const sleep = (ms = 2000) => new Promise((res) => setTimeout(res, ms));
+
+async function welcome() {
+  const rainbowTitle = chalkAnimation.rainbow(
+    'Are you a movie maniac? \n'
+  );
+
+  await sleep();
+  rainbowTitle.stop();
+
+  console.log(`${chalk.bgBlue('HOW TO PLAY')}
+  I am a proccess on your computer.
+  If you get any question wrong I will be ${chalk.bgRed('Terminate you!')}
+  So get all the questions right...
+
+  `);
+
+}
+
+await welcome();
+
